9fb3b8406f49c8973bb432d881310da98f6c67f1
ShulepovDen <shulepovdenis@mail.ru>
2020-12-28 18:39:27 +0300

    add 1 new test

--- src/tests/product-methods.test.js
+++ src/tests/product-methods.test.js
@@ -1,11 +1,6 @@
 /* eslint-disable no-undef */
 const moduleProduct = require('../script/product-methods');
 
-const productMethods = require('../utils/helpers/core');
-
-function plug() {
-  return 1;
-}
 test('check receipt of the count of each item', () => {
   const productElements = [
     {
@@ -198,52 +193,4 @@ test('check receipt of the name of the goods', () => {
   expect(productElements[2].name).not.toEqual('n');
   expect(productElements[2].name).not.toEqual(10);
   expect(productElements[2].name).not.toEqual('');
-});
-test('checking the created Observable Array', () => {
-  const productElements = [
-    {
-      id: 1,
-      name: 'Молоко',
-      count: 40,
-      priceForOne: 50,
-      priceTotal: 2000,
-    },
-    {
-      id: 2,
-      name: 'Хлеб',
-      count: 100,
-      priceForOne: 20,
-      priceTotal: 2000,
-    },
-    {
-      id: 3,
-      name: 'Лук',
-      count: 200,
-      priceForOne: 5,
-      priceTotal: 1000,
-    },
-  ];
-  let resultList = [];
-  resultList[0] = productMethods.createObservableObject(productElements[0], plug);
-  resultList[1] = productMethods.createObservableObject(productElements[1], plug);
-  resultList[2] = productMethods.createObservableObject(productElements[2], plug);
-  resultList = productMethods.createObservableArray(resultList, plug);
-  resultList[0].count = 1;
-  resultList[1].count = 1;
-  resultList[2].count = 1;
-  expect(resultList[0].count).toEqual(1);
-  expect(resultList[0].count).not.toEqual(2);
-  expect(resultList[0].count).not.toEqual('n');
-  expect(resultList[0].count).not.toEqual('');
-  expect(resultList[0].name).not.toEqual(1);
-  expect(resultList[1].count).toEqual(1);
-  expect(resultList[1].count).not.toEqual(2);
-  expect(resultList[1].count).not.toEqual('n');
-  expect(resultList[1].count).not.toEqual('');
-  expect(resultList[1].name).not.toEqual(1);
-  expect(resultList[2].count).toEqual(1);
-  expect(resultList[2].count).not.toEqual(2);
-  expect(resultList[2].count).not.toEqual('n');
-  expect(resultList[2].count).not.toEqual('');
-  expect(resultList[2].name).not.toEqual(1);
 });
